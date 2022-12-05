45641d947718bbd850b9a065bf2162ec461d06a0
AFei <laterfei@qq.com>
2022-12-05 11:24:58 +0800

    prettierd

--- lua/configs/nullls.lua
+++ lua/configs/nullls.lua
@@ -11,26 +11,31 @@ function M.config()
     end
 
     require("null-ls").setup({
-        -- diagnostics_format = "#{m} (#{s})",
-        -- sources = {
-        --     require("null-ls").builtins.code_actions.eslint_d.with({
-        --         condition = condition
-        --     }),
-        --     require("null-ls").builtins.formatting.eslint_d.with({
-        --         condition = condition
-        --     }),
-        --     require("null-ls").builtins.diagnostics.eslint_d.with({
-        --         condition = condition
-        --     }),
-        --
-        --     require("null-ls").builtins.diagnostics.tsc
-        --
-        --     -- require("null-ls").builtins.code_actions.eslint,
-        --     -- require("null-ls").builtins.formatting.eslint,
-        --     -- require("null-ls").builtins.diagnostics.eslint,
-        --     -- require("null-ls").builtins.diagnostics.tsc,
-        --     -- require("null-ls").builtins.formatting.prettierd
-        -- },
+        diagnostics_format = "#{m} (#{s})",
+        sources = {
+            -- require("null-ls").builtins.code_actions.eslint_d.with({
+            --     condition = condition
+            -- }),
+            -- require("null-ls").builtins.formatting.eslint_d.with({
+            --     condition = condition
+            -- }),
+            -- require("null-ls").builtins.diagnostics.eslint_d.with({
+            --     condition = condition
+            -- }),
+            --
+            -- require("null-ls").builtins.diagnostics.tsc
+
+            -- require("null-ls").builtins.code_actions.eslint,
+            -- require("null-ls").builtins.formatting.eslint,
+            -- require("null-ls").builtins.diagnostics.eslint,
+            -- require("null-ls").builtins.diagnostics.tsc,
+            -- require("null-ls").builtins.formatting.prettierd
+            require("null-ls").builtins.formatting.prettierd.with({
+              condition = function(utils)
+                return utils.root_has_file({ ".prettierrc"})
+              end,
+            }),
+        },
     })
 end
 
