9aaa6156dad229d067a7b13f8fd69fa45b8cd1a6
AFei <laterfei@qq.com>
2023-02-15 11:11:05 +0800

    delete-table-when-complete

--- lua/configs/cmp.lua
+++ lua/configs/cmp.lua
@@ -53,30 +53,6 @@ function M.config()
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = false,
             },
-            ["<Tab>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_next_item()
-                elseif luasnip.expand_or_jumpable() then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-                else
-                    fallback()
-                end
-            end, {
-                "i",
-                "s",
-            }),
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
-                    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                else
-                    fallback()
-                end
-            end, {
-                "i",
-                "s",
-            })
         },
         sources = {
             { name = "luasnip" },
