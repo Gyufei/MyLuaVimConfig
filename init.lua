290ee855dad1506c64c6d674e3615885e7ddd185
guoyufei <812773673@qq.com>
2026-06-12 16:51:08 +0800

    feat: replace-to-lazy-nvim

--- init.lua
+++ init.lua
@@ -68,53 +68,10 @@ vim.g.loaded_netrwPlugin       = 1
 vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_remote_plugins    = 1
 
-vim.cmd [[packadd packer.nvim]]
-
-require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use "andyg/leap.nvim"
-  use 'tpope/vim-surround'
-end)
-
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
-vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward)')
-vim.keymap.set({'n', 'x', 'o'}, 'gs', '<Plug>(leap-from-window)')
-
-do
-  local function ft(key_specific_args)
-    require('leap').leap(
-      vim.tbl_deep_extend('keep', key_specific_args, {
-        inputlen = 1,
-        inclusive = true,
-        opts = {
-          -- Force autojump.
-          labels = '',
-          -- Match the modes where you don't need labels (`:h mode()`).
-          safe_labels = vim.fn.mode(1):match('o') and '' or nil,
-        },
-      })
-    )
-  end
-
-  -- A helper function making it easier to set "clever-f" behavior
-  -- (using f/F or t/T instead of ;/, - see the plugin clever-f.vim).
-  local clever = require('leap.user').with_traversal_keys
-  local clever_f, clever_t = clever('f', 'F'), clever('t', 'T')
-
-  vim.keymap.set({ 'n', 'x', 'o' }, 'f', function()
-    ft { opts = clever_f }
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, 'F', function()
-    ft { backward = true, opts = clever_f }
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, 't', function()
-    ft { offset = -1, opts = clever_t }
-  end)
-  vim.keymap.set({ 'n', 'x', 'o' }, 'T', function()
-    ft { backward = true, offset = 1, opts = clever_t }
-  end)
-end
+require('config.lazy')
 
 if vim.g.vscode then
   require('vscode-conf')
-end;
+  return
+end
+
